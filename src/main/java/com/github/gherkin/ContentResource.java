9ceeb3f882ca9276452a710559bcc99a84e6679a
Gherkin <niyohn@stekligan.se>
2015-10-24 18:42:35 +0200

    article method

--- src/main/java/com/github/gherkin/ContentResource.java
+++ src/main/java/com/github/gherkin/ContentResource.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @Path("content")
@@ -14,8 +15,19 @@ public class ContentResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<String> fetchAll() {
-        return Arrays.asList("hello", "world");
+    public List<HashMap<String, String>> fetchAll() {
+        HashMap<String, String> content1 = createArticle("0", "Hej", "n[got smart");
+        HashMap<String, String> content2 = createArticle("0", "Eluceite", "n[got dumt");
+
+        return Arrays.asList(content1, content2);
+    }
+
+    private HashMap<String, String> createArticle(String value, String hej, String value1) {
+        HashMap<String, String> content = new HashMap<>();
+        content.put("id", value);
+        content.put("title", hej);
+        content.put("text", value1);
+        return content;
     }
 
 }

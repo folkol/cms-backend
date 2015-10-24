cd7b3ae1ac1cc454e7c4829faebf93669ebd4538
Gherkin <niyohn@stekligan.se>
2015-10-24 16:42:31 +0200

    Initial commit

---
+++ src/main/java/com/github/gherkin/BackendApplication.java
@@ -0,0 +1,9 @@
+package com.github.gherkin;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class BackendApplication extends Application {
+
+}

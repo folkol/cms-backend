5c6acd9aa100617362f225024b648bcf10e1f8b6
Gherkin <niyohn@stekligan.se>
2015-10-24 20:59:29 +0200

    added webapplicationexceptions

--- src/main/java/com/github/gherkin/ContentResource.java
+++ src/main/java/com/github/gherkin/ContentResource.java
@@ -7,7 +7,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -26,14 +28,18 @@ public class ContentResource {
     @GET
     @Path("{id}")
     public Article fetch(@PathParam("id") int id) {
-        return articles.get(id);
+        Article article = articles.get(id);
+        if(article == null) {
+            throw new WebApplicationException(Status.NOT_FOUND);
+        }
+        return article;
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Article insert(Article article) {
         if(article.getTitle() == null && article.getText() == null)
-            throw new IllegalArgumentException("missing title and body");
+            throw new WebApplicationException("title and boy missing", Status.BAD_REQUEST);
 
         article.setId(nextId.incrementAndGet());
         articles.put(article.getId(), article);

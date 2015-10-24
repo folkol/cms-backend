408b59f72fc344eca23b903bcf2db53886d092ff
Gherkin <niyohn@stekligan.se>
2015-10-24 19:18:53 +0200

    validation

--- src/main/java/com/github/gherkin/ContentResource.java
+++ src/main/java/com/github/gherkin/ContentResource.java
@@ -4,27 +4,33 @@ package com.github.gherkin;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("content")
 public class ContentResource {
+    static List<Article> articles = new ArrayList<Article>();
+    static AtomicInteger nextId = new AtomicInteger();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Article> fetchAll() {
-        Article content1 = new Article("hej", "hello");
-        Article content2 = new Article("eluveite", "havoc");
-
-
-        return Arrays.asList(content1, content2);
+        return articles;
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Article insert(Article article) {
+        if(article.getTitle() == null && article.getText() == null)
+            throw new IllegalArgumentException("missing title and body");
+
+        article.setId(nextId.incrementAndGet());
+        articles.add(article);
+
         return article;
     }
 }

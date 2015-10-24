3285f611955dad73e46655d616c9e4a7d0216c65
Gherkin <niyohn@stekligan.se>
2015-10-24 18:08:00 +0200

    barely anything

--- src/main/java/com/github/gherkin/ContentResource.java
+++ src/main/java/com/github/gherkin/ContentResource.java
@@ -1,17 +1,21 @@
 package com.github.gherkin;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Path("content")
 public class ContentResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String fetchAll() {
-        return "[]";
+    public List<String> fetchAll() {
+        return Arrays.asList("hello", "world");
     }
 
 }

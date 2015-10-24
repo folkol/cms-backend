05ea694aaa2b0f8bf010eab803e8d5de11bb1f14
Gherkin <niyohn@stekligan.se>
2015-10-24 23:05:56 +0200

    updated to generic content, more http methods

---
+++ src/main/java/com/github/gherkin/Content.java
@@ -0,0 +1,7 @@
+package com.github.gherkin;
+
+import java.util.HashMap;
+
+public class Content extends HashMap<String, String> {
+
+}
--- src/main/java/com/github/gherkin/ContentResource.java
+++ src/main/java/com/github/gherkin/ContentResource.java
@@ -2,14 +2,17 @@ package com.github.gherkin;
 
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -17,33 +20,58 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Produces(MediaType.APPLICATION_JSON)
 @Path("content")
 public class ContentResource {
-    static Map<Integer, Article> articles = new HashMap<>();
+    static Map<String, Content> contentMap = new HashMap<>();
     static AtomicInteger nextId = new AtomicInteger();
 
     @GET
-    public Map<Integer, Article> fetchAll() {
-        return articles;
+    public Map<String, Content> fetchAll() {
+        return contentMap;
     }
 
     @GET
     @Path("{id}")
-    public Article fetch(@PathParam("id") int id) {
-        Article article = articles.get(id);
-        if(article == null) {
+    public Content fetch(@PathParam("id") String id) {
+        Content content = contentMap.get(id);
+        if(content == null) {
             throw new WebApplicationException(Status.NOT_FOUND);
         }
-        return article;
+        return content;
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Article insert(Article article) {
-        if(article.getTitle() == null && article.getText() == null)
-            throw new WebApplicationException("title and boy missing", Status.BAD_REQUEST);
+    public Content insert(Content content) {
+        if(content == null)
+            throw new WebApplicationException("content missing", Status.BAD_REQUEST);
 
-        article.setId(nextId.incrementAndGet());
-        articles.put(article.getId(), article);
+        content.put("id", "" + nextId.incrementAndGet());
+        contentMap.put(content.get("id"), content);
 
-        return article;
+        return content;
     }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Content update(Content content) {
+        if(!content.containsKey("id"))
+            throw new WebApplicationException("missing id", Status.BAD_REQUEST);
+
+        contentMap.put(content.get("id"), content);
+
+        return content;
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Content remove(@PathParam("id") String id) {
+        if(!contentMap.containsKey(id)) {
+            throw new WebApplicationException(Status.NOT_FOUND);
+        }
+
+        Content content = contentMap.get(id);
+        contentMap.remove(id);
+
+        return content;
+    }
+
 }

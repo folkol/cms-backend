9282430ec175bcc4f9d225997d879f810116d3b3
Gherkin <niyohn@stekligan.se>
2015-10-24 19:07:38 +0200

    post, article class

--- src/main/java/com/github/gherkin/ContentResource.java
+++ src/main/java/com/github/gherkin/ContentResource.java
@@ -1,9 +1,7 @@
 package com.github.gherkin;
 
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 import java.util.Arrays;
@@ -15,19 +13,18 @@ public class ContentResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<HashMap<String, String>> fetchAll() {
-        HashMap<String, String> content1 = createArticle("0", "Hej", "n[got smart");
-        HashMap<String, String> content2 = createArticle("0", "Eluceite", "n[got dumt");
+    public List<Article> fetchAll() {
+        Article content1 = new Article("hej", "hello");
+        Article content2 = new Article("eluveite", "havoc");
+
 
         return Arrays.asList(content1, content2);
     }
 
-    private HashMap<String, String> createArticle(String value, String hej, String value1) {
-        HashMap<String, String> content = new HashMap<>();
-        content.put("id", value);
-        content.put("title", hej);
-        content.put("text", value1);
-        return content;
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Article insert(Article article) {
+        return article;
     }
-
 }

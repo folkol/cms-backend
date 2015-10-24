961417354956664040113d756f4c447ecddb6eed
Gherkin <niyohn@stekligan.se>
2015-10-24 20:33:09 +0200

    content hashmap

--- src/main/java/com/github/gherkin/ContentResource.java
+++ src/main/java/com/github/gherkin/ContentResource.java
@@ -1,35 +1,42 @@
 package com.github.gherkin;
 
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Produces(MediaType.APPLICATION_JSON)
 @Path("content")
 public class ContentResource {
-    static List<Article> articles = new ArrayList<Article>();
+    static Map<Integer, Article> articles = new HashMap<>();
     static AtomicInteger nextId = new AtomicInteger();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Article> fetchAll() {
+    public Map<Integer, Article> fetchAll() {
         return articles;
     }
 
+    @GET
+    @Path("{id}")
+    public Article fetch(@PathParam("id") int id) {
+        return articles.get(id);
+    }
+
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Article insert(Article article) {
         if(article.getTitle() == null && article.getText() == null)
             throw new IllegalArgumentException("missing title and body");
 
         article.setId(nextId.incrementAndGet());
-        articles.add(article);
+        articles.put(article.getId(), article);
 
         return article;
     }

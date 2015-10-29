0f094f9247901d4e242c03f79260c3dcb74bf0be
Gherkin <niyohn@stekligan.se>
2015-10-29 21:05:32 +0100

    renamed to Dao

--- src/main/java/com/github/gherkin/persistance/ContentDao.java
+++ src/main/java/com/github/gherkin/persistance/ContentDao.java
@@ -2,7 +2,7 @@ package com.github.gherkin.persistance;
 
 import com.github.gherkin.Content;
 
-public interface Persistance {
+public interface ContentDao {
     void save(Content content);
     Content retrieve(int id);
     void remove(int id);
